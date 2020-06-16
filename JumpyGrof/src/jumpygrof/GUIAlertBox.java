001ef2b2184898418ed67aaf9e5bcf3748d2ff10
owen0712 <63390980+owen0712@users.noreply.github.com>
2020-06-16 13:37:00 +0800

    Update GUIAlertBox.java

--- JumpyGrof/src/jumpygrof/GUIAlertBox.java
+++ JumpyGrof/src/jumpygrof/GUIAlertBox.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jumpygrof;
 
 import javafx.scene.control.Alert;
 
-/**
- *
- * @author USER
- */
 public class GUIAlertBox {
+    //pop out a window to show the error message
     public static void display(String warning,String detail){
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Warning");
